08aea3d4b1f5172469d3543e30cdac1fff36c60d
vlkavyampref9 <vlkavyampref9@gmail.com>
2022-05-29 23:12:08 +0530

    removed model component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CubeComponent } from './cube/cube.component';
-import { ModelComponent } from './model/model.component';
+
 
 const routes: Routes = [
   {
     path: "",
     component: CubeComponent
   },
-  {
-    path: "model",
-    component: ModelComponent
-  }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CubeComponent } from './cube/cube.component';
-import { ModelComponent } from './model/model.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent,
-    ModelComponent
+    CubeComponent    
   ],
   imports: [
     BrowserModule,
